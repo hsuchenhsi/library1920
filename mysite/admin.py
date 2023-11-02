cb8703d4527141fabc6ac05c14230624edc08a1a
hsuchenhsi <11056020@ntub.edu.tw>
2023-11-02 14:24:58 +0800

    css

--- mysite/admin.py
+++ mysite/admin.py
@@ -3,6 +3,6 @@ from mysite.models import Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display=('title','slug','pub_date')
+    list_display=('title','slug','author','date','img')
 
 admin.site.register(Post,PostAdmin)
--- mysite/models.py
+++ mysite/models.py
@@ -2,7 +2,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
     body = models.TextField()
+    date = models.CharField(max_length=200)
+    img = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
